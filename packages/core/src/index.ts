488eea04363046a30465dbdf5ed1bfc6632544b0
Diego De Sogos <diegodesogos@gmail.com>
2026-02-20 18:23:16 -0300

    fixing import errors in Vite runtime

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,13 @@
 // 1. Core SDK Facade
-export { QuozenClient, QuozenConfig } from "./QuozenClient";
+export { QuozenClient } from "./QuozenClient";
+export type { QuozenConfig } from "./QuozenClient";
 
 // 2. Domain Models & DTOs
 export * from "./domain";
 export { Ledger } from "./domain/Ledger";
 
 // 3. Configuration & Adapters (Required to instantiate QuozenClient)
-export { IStorageLayer } from "./infrastructure/IStorageLayer";
+export type { IStorageLayer } from "./infrastructure/IStorageLayer";
 export { GoogleDriveStorageLayer } from "./infrastructure/GoogleDriveStorageLayer";
 export { InMemoryAdapter } from "./storage/memory-adapter"; // Legacy/Test
 export { RemoteMockAdapter } from "./storage/remote-adapter"; // Legacy/Test
